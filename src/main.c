f4523ad3708db3e90439296b373b352e1881d4a2
Lachlan <lachlan@lachlandavidson.com>
2019-11-21 17:58:59 +1300

    LED Breathing

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
 #include <util/delay.h>
 
 // void set_a_high(bool on) {
@@ -14,21 +15,15 @@
 #define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
 #define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
 
-#define AH_BIT 3
-#define AL_BIT 2
+#define AH_BIT PD3
+#define AL_BIT PD2
 #define AH_PORT PORTD
 #define AL_PORT PORTD
 
-#define CH_BIT 5
-#define CL_BIT 1
+#define CH_BIT PD5
+#define CL_BIT PB1
 #define CH_PORT PORTD
 #define CL_PORT PORTB
-
-#define ah AH_PORT, AH_BIT
-#define al AL_PORT, AL_BIT
-#define ch CH_PORT, CH_BIT
-#define cl CL_PORT, CL_BIT
-
 
 void red_on() {
     DDRC |= (1 << 3);
@@ -46,49 +41,92 @@ void green_off() {
     DDRC &= ~(1 << 2);
 }
 
-void close() {
-    bitClear(AH_PORT, AH_BIT);
-    bitClear(AL_PORT, AL_BIT);
-    bitClear(CH_PORT, CH_BIT);
-    bitClear(CL_PORT, CL_BIT);
-}
-
-void fwd() {
-    close();
-    bitSet(AH_PORT, AH_BIT);
-    bitSet(CL_PORT, CL_BIT);
-}
-
-void rev() {
-    close();
-    bitSet(CH_PORT, CH_BIT);
-    bitSet(AL_PORT, AL_BIT);
+void set_duty(unsigned int duty) {
+    OCR1B = duty;
 }
 
 int main() {
-    
+    TIMSK |= _BV(TOIE1) | _BV(OCIE1B);
+    TCCR1B |= _BV(CS00);
+
+    sei();
+
     while (1) {
-        fwd();
-
-        green_off();
-        red_on();
-        
-        _delay_ms(2000);
-
-        rev();
-
-        red_off();
-        green_on();
-
-        _delay_ms(2000);
-
-        close();
-
-        red_off();
-        green_off();
-
-        _delay_ms(2000);
+        for(unsigned int i = 0; i < 40000; i += 10) {
+            set_duty(i);
+            _delay_ms(1);
+        }
+        for(unsigned int i = 39500; i > 0; i -= 10) {
+            set_duty(i);
+            _delay_ms(1);
+        }
     }
-    
-    return 0;
 }
+
+ISR (TIMER1_OVF_vect) {
+    red_on();
+}
+
+ISR (TIMER1_COMPB_vect) {
+    red_off();
+}
+
+// void __attribute__ ((noinline)) close() {
+//     bitClear(AH_PORT, AH_BIT);
+//     bitClear(AL_PORT, AL_BIT);
+//     bitClear(CH_PORT, CH_BIT);
+//     bitClear(CL_PORT, CL_BIT);
+// }
+
+// void fwd() {
+//     close();
+//     bitSet(AH_PORT, AH_BIT);
+//     bitSet(CL_PORT, CL_BIT);
+// }
+
+// void rev() {
+//     close();
+//     bitSet(CH_PORT, CH_BIT);
+//     bitSet(AL_PORT, AL_BIT);
+// }
+
+// void ioinit() {
+//     PORTC = 0;
+//     DDRC = _BV(PC2) & _BV(PC3); // TODOO FIXXX
+// }
+
+// int main() {
+//     ioinit();
+//     _delay_ms(5000);
+//     PORTC |= _BV(PC2);
+//     _delay_ms(1000);
+//     PORTC &= ~_BV(PC2);
+//     while (1);
+// }
+
+// int main() {
+//     while (1) {
+//         // fwd();
+//         bitSet(AH_PORT, AH_BIT);
+
+//         // green_off();
+//         // red_on();
+        
+//         // _delay_ms(2000);
+//         bitClear(AH_PORT, AH_BIT);
+
+//         // rev();
+
+//         // red_off();
+//         // green_on();
+
+//         // _delay_ms(2000);
+
+//         // close();
+
+//         // red_off();
+//         // green_off();
+
+//         _delay_ms(2000);
+//     }
+// }
