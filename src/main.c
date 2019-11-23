78c860e01c2c73415c5c98ba2a141a1ec30b61a5
Lachlan <lachlan@lachlandavidson.com>
2019-11-23 14:44:37 +1300

    RC in and bi-directional LED pwm working

--- src/main.c
+++ src/main.c
@@ -3,11 +3,6 @@
 #include <util/delay.h>
 #include <stdint.h>
 
-#define bitRead(value, bit) (((value) >> (bit)) & 0x01)
-#define bitSet(value, bit) ((value) |= (1UL << (bit)))
-#define bitClear(value, bit) ((value) &= ~(1UL << (bit)))
-#define bitWrite(value, bit, bitvalue) (bitvalue ? bitSet(value, bit) : bitClear(value, bit))
-
 #define AH_BIT PD3
 #define AL_BIT PD2
 #define AH_PORT PORTD
@@ -32,13 +27,22 @@
 #define GREEN_ON() (GREEN_LED_PORT &= ~(1 << GREEN_LED_BIT))
 #define GREEN_OFF() (GREEN_LED_PORT |= (1 << GREEN_LED_BIT))
 
-#define READ_RC() ((RC_IN_PIN & _BV(RC_IN_BIT)) ? 1 : 0)
+
+// 1000us
+#define PULSE_WIDTH_FLOOR 16000
+// 2000us
+#define PULSE_WIDTH_CEIL 32000
+
+#define PULSE_WIDTH_MAX 35000 
+#define PULSE_WIDTH_MIN 13000
+
+#define PULSE_WIDTH_NEUTRAL ((PULSE_WIDTH_CEIL + PULSE_WIDTH_FLOOR) / 2)
+#define PULSE_WIDRH_DEADBAND 1000
 
 void io_init() {
     DDRB &= ~(_BV(RC_IN_BIT));
-    // PORTB |= _BV(RC_IN_BIT);
+    DDRC |= _BV(RED_LED_BIT) | _BV(GREEN_LED_BIT);
 
-    DDRC |= (1 << 2) | (1 << 3);
     GREEN_OFF();
     RED_OFF();
 }
@@ -53,40 +57,52 @@ void icp_timer_init() {
 
 uint8_t overflow_count = 0;
 
-void set_duty(uint8_t);
+void set_power(int16_t);
 
 ISR (TIMER1_CAPT_vect) {
     // 1 = waiting for high, 0 = waiting for low
     static uint8_t edge = 1;
 
     if (edge) {
+        // reset counter
         TCNT1 = 0;
-        TCCR1B &= ~_BV(ICES1);
-    } else {
-        TCCR1B |= _BV(ICES1);
-        
-        uint32_t time = TCNT1;
-        uint32_t duty = 0;
+        // change to low interrupt
 
-        if (time < 17000 || time > 33000) {
+        TCCR1B &= ~_BV(ICES1);
+    } else if (overflow_count) {
+        // pulse lasted more than 1 overflow (1/1600000*2^16 seconds = 4096us) 
+        set_duty(0);
+    } else {
+        int32_t time = TCNT1;
+        int32_t duty = 0;
+
+        if (time < PULSE_WIDTH_MIN || time > PULSE_WIDTH_MAX) {
             duty = 0;
-        } else if (time > 32000) {
+        } else if (time >= PULSE_WIDTH_CEIL) {
             duty = 255;
-        } else if (time <= 16000) {
+        } else if (time <= PULSE_WIDTH_FLOOR) {
             duty = 0;
         } else {
-            duty = 255 * (time - 16000) / 16000;
+            duty = 510 * (PULSE_WIDTH_NEUTRAL - time) / (PULSE_WIDTH_CEIL - PULSE_WIDTH_FLOOR);
         }
 
-        set_duty(duty);
+        set_power(duty);
     }
 
-    edge = !edge;
     overflow_count = 0;
+
+    // toggle interrupt edge
+    TCCR1B ^= _BV(ICES1);
+
+    edge = !edge;
 }
 
+// 1/16000000*2^16 = ~4ms, normal period is 20ms, timeout after 100ms
 ISR (TIMER1_OVF_vect) {
-    set_duty(0);
+    if (overflow_count > 25)
+        set_power(0);
+    else
+        overflow_count = 0;
 }
 
 void pwm_timer_init() {
@@ -97,18 +113,56 @@ void pwm_timer_init() {
     TCCR2 |= _BV(CS20);
 }
 
+
+enum {
+    DIR_FWD,
+    DIR_STOPPED,
+    DIR_REV
+} direction = DIR_STOPPED;
+
 // output pwm on
 ISR (TIMER2_OVF_vect) {
-    if (OCR2) RED_ON();
+    switch (direction) {
+        case DIR_FWD:
+            GREEN_OFF();
+            RED_ON();
+            break;
+
+        case DIR_STOPPED:
+            GREEN_OFF();
+            RED_OFF();
+            break;
+
+        case DIR_REV:
+            RED_OFF();
+            GREEN_ON();
+            break;
+    }
 }
 
 // output pwn off
 ISR (TIMER2_COMP_vect) {
     RED_OFF();
+    GREEN_OFF();
 }
 
 void set_duty(uint8_t duty) {
     OCR2 = duty;
+}
+
+void set_power(int16_t power) {
+    cli();
+    if (power > 0) {
+        direction = DIR_FWD;
+        set_duty(power);
+    } else if (power < 0) {
+        direction = DIR_REV;
+        set_duty(-power);
+    } else {
+        direction = DIR_STOPPED;
+        set_duty(0);
+    }
+    sei();
 }
 
 int main() {
@@ -122,71 +176,3 @@ int main() {
 
     while (1) {}
 }
-
-// ISR (TIMER1_OVF_vect) {
-//     red_on();
-// }
-
-// ISR (TIMER1_COMPB_vect) {
-//     red_off();
-// }
-
-// void __attribute__ ((noinline)) close() {
-//     bitClear(AH_PORT, AH_BIT);
-//     bitClear(AL_PORT, AL_BIT);
-//     bitClear(CH_PORT, CH_BIT);
-//     bitClear(CL_PORT, CL_BIT);
-// }
-
-// void fwd() {
-//     close();
-//     bitSet(AH_PORT, AH_BIT);
-//     bitSet(CL_PORT, CL_BIT);
-// }
-
-// void rev() {
-//     close();
-//     bitSet(CH_PORT, CH_BIT);
-//     bitSet(AL_PORT, AL_BIT);
-// }
-
-// void ioinit() {
-//     PORTC = 0;
-//     DDRC = _BV(PC2) & _BV(PC3); // TODOO FIXXX
-// }
-
-// int main() {
-//     ioinit();
-//     _delay_ms(5000);
-//     PORTC |= _BV(PC2);
-//     _delay_ms(1000);
-//     PORTC &= ~_BV(PC2);
-//     while (1);
-// }
-
-// int main() {
-//     while (1) {
-//         // fwd();
-//         bitSet(AH_PORT, AH_BIT);
-
-//         // green_off();
-//         // red_on();
-        
-//         // _delay_ms(2000);
-//         bitClear(AH_PORT, AH_BIT);
-
-//         // rev();
-
-//         // red_off();
-//         // green_on();
-
-//         // _delay_ms(2000);
-
-//         // close();
-
-//         // red_off();
-//         // green_off();
-
-//         _delay_ms(2000);
-//     }
-// }
